b280a79d68b818771a1ee33b95afcc14b11ee4d7
kshitija-star <deshmukhkshitija08@gmail.com>
2021-04-08 19:41:14 +0530

    first commit

---
+++ TestGit/src/HelloWord.java
@@ -0,0 +1,12 @@
+
+public class HelloWord {
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("kshitija");
+		System.out.println("kshitija,good day");
+		
+	}
+	}
+
+
